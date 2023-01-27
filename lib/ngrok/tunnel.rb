73fd54b50bc4a0ad16d86f6e0b7bf1ee6bf13a2a
Cody Moorhouse <cody.m@getjobber.com>
2023-01-27 10:30:13 -0700

    Remove const, make it generalized

--- lib/ngrok/tunnel.rb
+++ lib/ngrok/tunnel.rb
@@ -7,18 +7,14 @@ module Ngrok
   class FetchUrlError < StandardError; end
   class Error < StandardError; end
 
-
-  ALLOW_HOSTS = [
-    "localhost.test"
-  ].freeze
-
   class Tunnel
 
     class << self
       attr_reader :pid, :ngrok_url, :ngrok_url_https, :status
 
       def init(params = {})
-        @params = {addr: 3001, timeout: 10, config: create_empty_config_file.path}.merge(params)
+        allow_hosts = params[:allow_hosts] || []
+        @params = {addr: 3001, timeout: 10, config: create_empty_config_file(allow_hosts).path}.merge(params)
         @status = :stopped unless @status
       end
 
@@ -133,11 +129,11 @@ module Ngrok
         /^ngrok version [012]\./.match?(@version)
       end
 
-      def create_empty_config_file
+      def create_empty_config_file(allow_hosts)
         tmp = Tempfile.new('ngrok_config-')
         tmp << "version: ""2""\n"
         tmp << "web_allow_hosts:\n"
-        ALLOW_HOSTS.each {|host| tmp << "  - #{host}\n"}
+        allow_hosts.each {|host| tmp << "  - #{host}\n"}
         tmp.flush
         tmp.close
         tmp
